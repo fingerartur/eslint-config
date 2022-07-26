86d92e132ce130b22f159a53b02f6d2089457d3e
Artur Finger <artur.finger@nangu.tv>
2022-07-26 12:33:09 +0200

    Formatted index.js

--- index.js
+++ index.js
@@ -2,120 +2,120 @@
  * Eslint config for typescript web apps and libs
  */
 module.exports = {
-  "parser": "@typescript-eslint/parser",
-  "ignorePatterns": [
+  parser: '@typescript-eslint/parser',
+  ignorePatterns: [
     // text files
-    "**/*.md",
-    "**/*.json",
-    "**/*.xml",
-    "**/*.txt",
-    "**/*.drawio",
+    '**/*.md',
+    '**/*.json',
+    '**/*.xml',
+    '**/*.txt',
+    '**/*.drawio',
     // libs
-    "**/node_modules/",
-    "**/*.lock",
+    '**/node_modules/',
+    '**/*.lock',
     // dist files
-    "**/dist/",
-    "**/lib/",
-    "**/out/",
+    '**/dist/',
+    '**/lib/',
+    '**/out/',
     // docs
-    ".doc/",
-    "doc/",
-    "docs/",
+    '.doc/',
+    'doc/',
+    'docs/',
     // envs
-    "**/.env",
-    "**/.gitignore",
+    '**/.env',
+    '**/.gitignore',
   ],
-  "plugins": [
-    "@typescript-eslint",
-    "import"
+  plugins: [
+    '@typescript-eslint',
+    'import',
   ],
-  "extends": [
+  extends: [
     /**
      * Prevent people from writing extremely convoluted or nonsensical JS/TS which is syntactically correct,
      * yes, but only a madman would code this way.
      */
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     /**
      * Sorting and dedupe of module imports
      */
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript"
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
   ],
-  "env": {
-    "browser": true
+  env: {
+    browser: true,
   },
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "ecmaVersion": 2020,
-    "sourceType": "module",
-    "project": "./tsconfig.json",
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json',
   },
-  "rules": {
+  rules: {
     // Basic formatting (something like prettier) ----------------------------
     // no semicolons
-    "semi": ["error", "never"],
+    semi: ['error', 'never'],
     // indentation 2 spaces
-    "indent": ["error",
+    indent: ['error',
       2,
       {
-        "SwitchCase": 1
-      }
+        SwitchCase: 1,
+      },
     ],
-    "@typescript-eslint/no-namespace": "off",
+    '@typescript-eslint/no-namespace': 'off',
     // max line length 160 chars
-    "max-len": [
-      "warn",
-      160
+    'max-len': [
+      'warn',
+      160,
     ],
     // enforce dangling comma
-    "comma-dangle": [
-      "error",
-      "always-multiline"
+    'comma-dangle': [
+      'error',
+      'always-multiline',
     ],
     // single quotes only
-    "quotes": ["error", "single"],
+    quotes: ['error', 'single'],
     // enforce curly braces for functions, ifs, etc.
-    "curly": ["warn", "all"],
-    "object-curly-spacing": ["warn", "always"],
-    "comma-spacing": ["warn",
+    curly: ['warn', 'all'],
+    'object-curly-spacing': ['warn', 'always'],
+    'comma-spacing': ['warn',
       {
-        "after": true
-      }
+        after: true,
+      },
     ],
 
     // Module imports -------------------------------------------------------
-    "import/order": [
-      "warn",
+    'import/order': [
+      'warn',
       {
         // Ordering of what types/groups of imports come first and what next
-        "groups": [
-          "builtin",
-          "external",
-          "internal",
-          "parent",
-          "sibling",
-          "index",
-          "object",
-          "type"
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+          'type',
         ],
-        "newlines-between": "always",
+        'newlines-between': 'always',
         // Make sure imports are ordered alphabetically
-        "alphabetize": {
-          "order": "asc",
-          "caseInsensitive": true
-        }
-      }
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
     ],
-    "import/newline-after-import": [
-      "warn",
+    'import/newline-after-import': [
+      'warn',
       {
-        "count": 1
-      }
+        count: 1,
+      },
     ],
 
     // Typescript config -----------------------------------
@@ -128,19 +128,19 @@ module.exports = {
      *    b: string
      *  }
      */
-    "@typescript-eslint/member-delimiter-style": ["error", {
-      "multiline": {
-        "delimiter": "none",
+    '@typescript-eslint/member-delimiter-style': ['error', {
+      multiline: {
+        delimiter: 'none',
       },
-      "singleline": {
-        "delimiter": "semi",
+      singleline: {
+        delimiter: 'semi',
       },
     }],
     // Allow return types to be auto-inferred
-    "@typescript-eslint/explicit-function-return-type": "off",
+    '@typescript-eslint/explicit-function-return-type': 'off',
     // Allow types to be auto-inferred across files
-    "@typescript-eslint/explicit-module-boundary-types": "off",
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     // Sometimes you need empty functions
-    "@typescript-eslint/no-empty-function": "off",
+    '@typescript-eslint/no-empty-function': 'off',
   },
 }
